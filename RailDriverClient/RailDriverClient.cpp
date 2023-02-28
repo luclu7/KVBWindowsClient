de9148ea8ef77a7a6ef41735ccc2f0f47a1a21f8
Lucie Delestre <me@luclu7.fr>
2023-02-28 23:20:22 +0100

    RailDriverClient: ajout exemple lecture de fichier de config des contrôles

--- RailDriverClient/RailDriverClient.cpp
+++ RailDriverClient/RailDriverClient.cpp
@@ -2,7 +2,15 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+#include <iomanip>
+
 #include "RailDriver.h"
+
+std::vector<RailDriverClass::Control> readConfigFile(const std::string& filename);
 
 int main()
 {
@@ -12,22 +20,63 @@ int main()
 
 	Sleep(1000);
 	
-	std::vector<RailDriverClass::Control> controls = rd.GetControls();
+	std::vector<RailDriverClass::Control> controls = readConfigFile("6500_aigu.ini");
 
-	for (RailDriverClass::Control control : controls) {
-		std::cout << control.id << ": " << control.name << " (" << control.minValue << " -> " << control.maxValue << ")" << std::endl;
+	// create a map consisting of the control id and the last control value
+	std::map<int, float> controlValues;
+	for (auto& control : controls) {
+		controlValues[control.id] = control.id;
+	}
+
+	std::cout << std::setprecision(2) << std::fixed;	
+
+	while (true) {
+		rd.SetRailDriverConnected(true);
+		
+		// monitore the controls
+		for (auto& control : controls) {
+			float value = rd.GetCurrentControllerValue(control.id);
+
+			if (value != controlValues[control.id]) {
+				// print the value as 2f float
+				std::cout << "Control " << control.name << " = " << value << std::endl;
+				controlValues[control.id] = value;
+			}
+		}
+
+		std::cout << std::endl;
+		
+
+		Sleep(500);
 	}
 
 	
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+std::vector<RailDriverClass::Control> readConfigFile(const std::string& filename) {
+	std::vector<RailDriverClass::Control> controls;
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+	std::ifstream file(filename);
+	if (!file.is_open()) {
+		std::cerr << "Error opening file " << filename << std::endl;
+		return controls;
+	}
+
+	std::string line;
+	while (std::getline(file, line)) {
+		auto pos = line.find('=');
+		if (pos != std::string::npos) {
+			auto controlId = line.substr(0, pos);
+			auto controlName = line.substr(pos + 1);
+
+			std::cout << "Control " << controlId << " = " << controlName << std::endl;
+
+			RailDriverClass::Control control;
+			control.id = std::stoi(controlId);
+			control.name = controlName;
+			controls.push_back(control);
+		}
+	}
+
+	return controls;
+}
